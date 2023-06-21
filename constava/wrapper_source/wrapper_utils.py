964d18963810c83e6bb40b9a27147c555549802c
Jose Gavalda Garcia <jose.gavalda.garcia@vub.be>
2023-06-21 16:08:25 +0200

    wrapper_utils.py updated with new flags, basic functionality tested but not yet the kde dump and load. Needed to go for the wrap up

--- constava/wrapper_source/wrapper_utils.py
+++ constava/wrapper_source/wrapper_utils.py
@@ -1,6 +1,7 @@
 # import sys
 # import argparse
 import os
+import glob
 
 from constava.calculator import ConfStateCalculator
 from constava.ensemblereader import EnsembleReader
@@ -15,47 +16,48 @@ class ConStaVa:
 		self.pdfestimator = None
 		self.ensemble = None
 		self.kdes = None
-		self.training_data = None
-		self.kde_degrees = False
-		self.dump_kde = None
+		self.kde_from_data = None
+		self.kde_from_degrees = False
+		self.dump_kdes = None
 		self.bootstrap = None
 		self.bootstrap_samples = 500
 		self.window = None
-		self.infile = None
+		self.input_file = None
 		self.input_format = 'auto'
 		self.output_file = None
 		self.output_format = 'auto'
-		self.degrees = False
-		self.quick = False
+		self.input_degrees = False
+		# self.quick = False
 		self.precision = 4
 		self.kde_bandwidth = 0.13
 
 	def train_kde(self, training_data=None, kde_degrees=False):
-		self.training_data = training_data
-		self.kde_degrees = kde_degrees
+		self.kde_from_data = training_data
+		self.kde_from_degrees = kde_degrees
 		self.fit_kde()
 
 	# Read input files
 	def read_input_files(self, infile=None, degrees=False, input_format='auto'):
-		self.infile = [os.path.join(infile, the_file) for the_file in os.listdir(infile)]
-		self.degrees = degrees
+		# self.infile = [os.path.join(infile, the_file) for the_file in os.listdir(infile)]
+		self.input_file = glob.glob(infile)
+		self.input_degrees = degrees
 		self.input_format = input_format
 
 		reader = EnsembleReader(filetype_str=self.input_format,
-		                        degrees2radians=self.degrees)
-		self.ensemble = reader.readFiles(*self.infile)
+		                        degrees2radians=self.input_degrees)
+		self.ensemble = reader.readFiles(*self.input_file)
 
 	def load_kde(self):
 		self.pdfestimator = KDEStatePdf.from_pickle(self.kdes)
 
 	def fit_kde(self):
 		self.pdfestimator = KDEStatePdf.from_fitting(
-			self.training_data,
+			self.kde_from_data,
 			bandwidth=self.kde_bandwidth,
-			degrees2radians=self.degrees)
+			degrees2radians=self.input_degrees)
 
 	def save_kde(self):
-		self.pdfestimator.dump_pickle(self.dump_kde)
+		self.pdfestimator.dump_pickle(self.dump_kdes)
 
 	def calculate_results(self):
 		# Load the calculation methods
