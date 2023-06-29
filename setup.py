3ba1f601fd963bc451f27ac350670957d33a26a6
Jose Gavalda Garcia <jose.gavalda.garcia@vub.be>
2023-06-29 17:45:04 +0200

    explicitly includes the python versions on the setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,11 @@ setup(
     include_package_data=True,
     classifiers=[
         "Natural Language :: English",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
