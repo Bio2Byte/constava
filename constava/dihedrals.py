ff389b5a1b96ea62c91014832a8ead7767495c81
David BICKEL <david.bickel@vub.be>
2023-08-01 13:02:00 +0000

    Merged in dihedrals_script (pull request #12) * Make dihedral calculation a stand-alone function that can be imported as needed
    Approved-by: Jose Gavalda Garcia

--- constava/dihedrals.py
+++ constava/dihedrals.py
@@ -2,29 +2,42 @@
 phi/psi backbone dihedral angles from a conformational ensmeble. """
 
 import argparse, os, sys
-import csv
 from typing import List, NamedTuple
 
 import MDAnalysis as mda
 from MDAnalysis.analysis.dihedrals import Ramachandran
 import numpy as np
+import pandas as pd
 
 
-def parse_arguments(arguments: List[str]) -> NamedTuple:
-    ''' Parse command line arguments '''
+def parse_arguments(cmdline_arguments: List[str]) -> NamedTuple:
+    """Parses the command line arguments and does some minor sanity checking.
+    
+    Parameters:
+    -----------
+        cmdline_arguments : List[str]
+            Parses the command line arguments provided as a list of strings, as
+            returned by `sys.argv`
+    
+    Returns:
+    --------
+        args : NameSpace
+            Object containing the parsed parameters.    
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--structure",  
-        help=("Structure file with atomic information: [pdb, gro, tpr]"))
+        help="Structure file with atomic information: [pdb, gro, tpr]")
     parser.add_argument("-f", "--trajectory", nargs="+", 
-        help=("Trajectory file with coordinates: [pdb, gro, trr, xtc, crd, nc]"))
+        help="Trajectory file with coordinates: [pdb, gro, trr, xtc, crd, nc]")
     parser.add_argument("-o", "--outfile", default=None, required=False,
-        help=("(Optional) CSV file to write dihedral information to. Default: dihedrals.csv"))
+        help="(Optional) CSV file to write dihedral information to. Default: dihedrals.csv")
     parser.add_argument("--selection", default="protein",
-        help=("(Optional) Selection for the dihedral calculation. Default: 'protein'"))
+        help="(Optional) Selection for the dihedral calculation. Default: 'protein'")
     parser.add_argument("--precision", default=5, type=int,
-        help=("(Optional) Defines the number of decimals written for the dihedrals "
-              "(in radians). Default: 5"))
-    args = parser.parse_args(arguments)
+        help="(Optional) Defines the number of decimals written for the dihedrals. Default: 5")
+    parser.add_argument("--degrees", action="store_true",
+        help="Results are written in degrees instead of radians. Default: radians")
+    args = parser.parse_args(cmdline_arguments)
 
     if args.outfile is None:
         args.outfile = "dihedrals.csv"
@@ -35,28 +48,65 @@ def parse_arguments(arguments: List[str]) -> NamedTuple:
                 ))
     return args
 
-def main():
-    # Parse command line arguments
-    args = parse_arguments(sys.argv[1:])
-    # Load conformational ensemble
-    u = mda.Universe(args.structure, args.trajectory)
+def calculate_dihedrals(structure: str, trajectory: List[str], selection: str = "protein", degree: bool = False):
+    """Calculates the backbone dihedral angles and returns the result as a 
+    DataFrame.
+    
+    Parameters:
+    -----------
+        structure : str
+            Path to the structure File to use [*.gro, *.pdb, *.tpr, ...]
+        trajectory : List[str]
+            List of paths to trajectory files corresponding to the structure 
+            file [*.trr, *.xtc, ...]
+        selection : str
+            Atom selection for which the dihedral angles should be calculated. 
+            Corresponds to the MDAnalysis selection syntax. [default: `protein`]
+        degrees : bool
+            If True dihedrals are reported in degrees, else in radians. 
+            [default: False]
+    
+    Returns:
+    --------
+        dihedrals : DataFrame
+            DataFrame containing the backbone dihedral angles calculated along 
+            the trajectory
+    """
+    # Load trajectory files
+    u = mda.Universe(structure, trajectory)
     # Calculate dihedral angles.
-    rama = Ramachandran(u.select_atoms(args.selection))
+    rama = Ramachandran(u.select_atoms(selection))
     rama.run()
-    dihedrals = np.radians(rama.results.angles)
+    if degree:
+        phicol, psicol = "Phi[deg]", "Psi[deg]"
+        results = rama.results.angles
+    else:
+        phicol, psicol = "Phi[rad]", "Psi[rad]"
+        results = np.radians(rama.results.angles)
+    # Convert resulting numpy array into a comprehensive DataFrame
+    dihedrals = pd.DataFrame(columns = ["#Frame", "ResIndex", "ResName", phicol, psicol])
+    # Iterate over selection (C-alphas) used to define the dihedrals
+    for i, atom in enumerate(rama.ag3):
+        # Select results for given
+        df = pd.DataFrame({
+            "#Frame": rama.frames,
+            "ResIndex": np.full(rama.frames.shape, atom.resid, dtype=int),
+            "ResName": np.full(rama.frames.shape, atom.resname),
+            phicol: results[:,i,0], 
+            psicol: results[:,i,1]})
+        dihedrals = pd.concat([dihedrals, df], ignore_index=True)
+    return dihedrals
 
-    # Write the output
-    float2str = lambda x: "{0:.{1}f}".format(x, args.precision)
-    with open(args.outfile, "w") as ohandle:
-        writer = csv.writer(ohandle)
-        writer.writerow(["#Frame", "ResIndex", "ResName", "Phi[rad]", "Psi[rad]"])
-        # Iterate over C-alphas defining the dihedrals
-        for i, atom in enumerate(rama.ag3):
-            resname = atom.resname
-            resid = atom.resid
-            for j, frame in enumerate(rama.frames):
-                phi, psi = dihedrals[j,i,:]
-                writer.writerow([frame, resid, resname, float2str(phi), float2str(psi)])
+def main(cmdline_arguments: List[str]):
+    """Main function executed when the script is run from the command line"""
+    # Parse command line arguments
+    args = parse_arguments(cmdline_arguments)
+    # Calculate dihedrals
+    dihedrals = calculate_dihedrals(
+        args.structure, args.trajectory, args.selection, args.degrees)
+    # Write results
+    float2str = f"%.{args.precision}f" # Definition of float format in output
+    dihedrals.to_csv(args.outfile, header=True, index=False, float_format=float2str)
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(main(sys.argv[1:]))
